5144da5b7129dec31e66813deed249653491f320
ALLO123 <109481771+LeoCode23@users.noreply.github.com>
2022-08-25 07:35:24 -0400

    UI1

---
+++ GUI.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+//class GUI
+public class GUI
+{
+    //method main
+    public static void main(String[] args)
+    {
+    System.out.println("1 + 1 = " + (1 + 1));
+    }
+    
+    
+    }
